cd91f0a586d9cc0e49a28c7bfa9112c898171070
Myrotvorets <support@myrotvorets.center>
2020-05-30 12:38:31 +0300

    Fix enqueue logic + debug

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -23,59 +23,70 @@ function notify(e: Error): void {
     error(e);
 }
 
-async function processOne(this: BotContext): Promise<void> {
+function processOne(this: BotContext): void {
     const keys = Object.keys(queue);
     if (!keys.length) {
+        dbg('processOne: queue is empty');
         return;
     }
 
     const postId = keys[0];
     const item = queue[postId];
-    delete queue[postId];
 
-    const stats = await getPostStats(this.db, +postId);
-    const oldMarkup = item.reply_markup;
-    const newMarkup = { inline_keyboard: buildInlineKeyboardFromMarkup(oldMarkup, stats, +postId) };
-    item.reply_markup = newMarkup;
+    dbg('About to process %d', postId);
 
-    this.telegram
-        .callApi('editMessageReplyMarkup', item)
-        .then((): unknown => setImmediate(processOne.bind(this)))
-        .catch((e: Error): void => {
-            if (e.constructor.name === 'TelegramError') {
-                if ((e as any).code === 429) {
-                    const payload = (e as any).on?.payload as QueuedItem | undefined;
-                    const timeout = (e as any).parameters?.retry_after as number | undefined;
+    getPostStats(this.db, +postId)
+        .then((stats): void => {
+            dbg('Stats for post %d: %o', +postId, stats);
+            delete queue[postId];
 
-                    if (payload && timeout) {
-                        if (queue[postId] === undefined) {
-                            queue[postId] = payload;
-                            dbg('will retry editMessageReplyMarkup for post %d in %d seconds', +postId, timeout);
-                            setTimeout(processOne.bind(this), timeout * 1000);
-                        } else {
-                            dbg('not queueing additional editMessageReplyMarkup for post %d', +postId);
+            const oldMarkup = item.reply_markup;
+            const newMarkup = { inline_keyboard: buildInlineKeyboardFromMarkup(oldMarkup, stats, +postId) };
+            item.reply_markup = newMarkup;
+
+            this.telegram
+                .callApi('editMessageReplyMarkup', item)
+                .then((): unknown => setImmediate(processOne.bind(this)))
+                .catch((e: Error): void => {
+                    if (e.constructor.name === 'TelegramError') {
+                        if ((e as any).code === 429) {
+                            const payload = (e as any).on?.payload as QueuedItem | undefined;
+                            const timeout = (e as any).parameters?.retry_after as number | undefined;
+
+                            if (payload && timeout) {
+                                if (queue[postId] === undefined) {
+                                    queue[postId] = payload;
+                                }
+
+                                dbg('will retry editMessageReplyMarkup for post %d in %d seconds', +postId, timeout);
+                                setTimeout(processOne.bind(this), timeout * 1000);
+                                return;
+                            }
+                        } else if ((e as any).code === 400) {
+                            dbg('Skipping error 400 from editMessageReplyMarkup: %o', e);
+                            setImmediate(processOne.bind(this));
+                            return;
                         }
-
-                        return;
                     }
-                } else if ((e as any).code === 400) {
-                    dbg('Skipping error 400 from editMessageReplyMarkup');
-                    return;
-                }
-            }
 
-            notify(e);
-        });
+                    setImmediate(processOne.bind(this));
+                    notify(e);
+                });
+        })
+        .catch((e) => notify(e));
 }
 
 export function editMessageReplyMarkup(context: BotContext, postId: number): void {
     const callNow = Object.keys(queue).length === 0;
-    queue[postId] = {
+    const entry = {
         chat_id: context.chat?.id,
-        message_id: context.message?.message_id,
+        message_id: context.callbackQuery?.message?.message_id,
         inline_message_id: context.callbackQuery?.inline_message_id,
         reply_markup: context.callbackQuery?.message?.reply_markup,
     };
+
+    dbg('Queueing %o', entry);
+    queue[postId] = entry;
 
     if (callNow) {
         setImmediate(processOne.bind(context));
