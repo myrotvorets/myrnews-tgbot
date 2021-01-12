e1508bb8fb644988c691ad5556bcbb42e86f1f24
Myrotvorets <support@myrotvorets.center>
2021-01-12 19:47:06 +0200

    Replace axios with node-fetch to match Telegraf's deps

--- src/lib/wpapi.ts
+++ src/lib/wpapi.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import fetch from 'node-fetch';
 import type { PostData } from '../types';
 
 interface WPPost {
@@ -19,26 +19,27 @@ interface WPMedia {
     };
 }
 
+async function get<T>(url: string): Promise<T> {
+    const raw = await fetch(url);
+    return (await raw.json()) as T;
+}
+
 export async function getPosts(baseUrl: string): Promise<PostData[]> {
-    const result: PostData[] = [];
-    const posts = await axios.get<WPPost[]>(`${baseUrl}/wp-json/wp/v2/posts`);
-    for (let i = 0; i < posts.data.length; ++i) {
-        const post = posts.data[i];
-        result.push({
+    const posts = await get<WPPost[]>(`${baseUrl}/wp-json/wp/v2/posts`);
+    return posts.map(
+        (post): PostData => ({
             id: post.id,
             link: post.link,
             title: post.title.rendered,
             excerpt: post.excerpt.rendered.replace(/<[^>]+>/gu, ''),
             featuredMedia: post.featured_media,
-        });
-    }
-
-    return result;
+        }),
+    );
 }
 
 export async function getFeaturedImageUrl(baseUrl: string, id: number): Promise<string> {
-    const media = await axios.get<WPMedia>(`${baseUrl}/wp-json/wp/v2/media/${id}`);
-    const link = media.data.guid.rendered;
+    const media = await get<WPMedia>(`${baseUrl}/wp-json/wp/v2/media/${id}`);
+    const link = media.guid.rendered;
     try {
         const url = new URL(link);
         return url.href;
