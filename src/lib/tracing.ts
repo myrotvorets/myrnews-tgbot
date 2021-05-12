c94dec7e72b1ed41257db7dc65482533da8b984b
Myrotvorets <support@myrotvorets.center>
2021-05-13 02:14:11 +0300

    Temporarily disable ot-telegraf

--- src/lib/tracing.ts
+++ src/lib/tracing.ts
@@ -3,7 +3,7 @@
 import { EventEmitter } from 'events';
 import { OpenTelemetryConfigurator } from '@myrotvorets/opentelemetry-configurator';
 import { KnexInstrumentation } from '@myrotvorets/opentelemetry-plugin-knex';
-import { TelegrafPlugin } from './ot-telegraf';
+// import { TelegrafPlugin } from './ot-telegraf';
 
 if (+(process.env.ENABLE_TRACING || 0)) {
     EventEmitter.defaultMaxListeners += 5;
@@ -13,7 +13,7 @@ export async function configure(): Promise<void> {
     if (+(process.env.ENABLE_TRACING || 0)) {
         const configurator = new OpenTelemetryConfigurator({
             serviceName: 'bot/myrotvorets.news',
-            instrumentations: [new KnexInstrumentation(), new TelegrafPlugin()],
+            instrumentations: [new KnexInstrumentation() /* , new TelegrafPlugin()*/],
         });
 
         await configurator.start();
